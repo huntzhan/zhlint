999aaf132c79b4299ca771091beafd3965234a40
huntzhan <programmer.zhx@gmail.com>
2016-09-03 15:20:16 +0800

    init.

---
+++ zh_doclint/metadata.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+
+
+NAME = 'zh_doclint'
+VERSION = '0.1.0'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = 'automated checker of chinese document.'
